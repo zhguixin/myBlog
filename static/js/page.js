3b2f30355772772ab26152f8b6e0a49fb20d9d93
zhguixin <zhguixin@163.com>
2018-01-09 23:36:01 +0800

    change the tag's page style

--- static/js/page.js
+++ static/js/page.js
@@ -28,6 +28,23 @@ $(function(){
  $(window).resize(footerPosition);
 });
 
+//标签页展开、隐藏按钮功能实现
+$(document).ready(function(){
+    $("#closeAll").click(function(){
+        for(var i=1;i < 99; i++) {
+            $('#'+i).collapse('hide');
+        }
+      });
+});
+
+$(document).ready(function(){
+    $("#openAll").click(function(){
+        for(var i=1;i < 99; i++) {
+            $('#'+i).collapse('show');
+        }
+      });
+});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
 }
