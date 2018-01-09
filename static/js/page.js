a51af9b9655de044ca722884fa8be5111ce82279
zhguixin <zhguixin@163.com>
2018-01-09 22:17:17 +0800

    Fix Bug:fixed footer position

--- static/js/page.js
+++ static/js/page.js
@@ -11,6 +11,23 @@ $('#myBlog').on('click', function (e) {
     }, 700);
 });
 
+//固定顶部footer，动态添加【fixed-bottom】样式
+$(function(){
+ function footerPosition(){
+     $("#footer").removeClass("fixed-bottom");
+     var contentHeight = document.body.scrollHeight,//网页正文全文高度
+         winHeight = window.innerHeight;//可视窗口高度，不包括浏览器顶部工具栏
+     if(!(contentHeight > winHeight)){
+         //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
+         $("#footer").addClass("fixed-bottom");
+     } else {
+         $("#footer").removeClass("fixed-bottom");
+     }
+ }
+ footerPosition();
+ $(window).resize(footerPosition);
+});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
 }
