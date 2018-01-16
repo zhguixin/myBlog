6cd98fd8bcd05fa8264a832dc7cf9636613e44a2
zhguixin <zhguixin@163.com>
2018-01-16 23:24:20 +0800

    template optimization and add top progressbar

--- static/js/page.js
+++ static/js/page.js
@@ -20,20 +20,3 @@ $(function(){
  footerPosition();
  $(window).resize(footerPosition);
 });
-
-//标签页展开、隐藏按钮功能实现
-$(document).ready(function(){
-    $("#closeAll").click(function(){
-        for(var i=1;i < 99; i++) {
-            $('#'+i).collapse('hide');
-        }
-      });
-});
-
-$(document).ready(function(){
-    $("#openAll").click(function(){
-        for(var i=1;i < 99; i++) {
-            $('#'+i).collapse('show');
-        }
-      });
-});
